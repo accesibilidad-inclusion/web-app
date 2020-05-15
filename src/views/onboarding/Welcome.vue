e6d05127dd2e322b1b96c4db370145d3be490e09
Basilio Cáceres <basilio@bloom-ux.com>
2020-05-15 16:11:50 -0400

    se homologa color de fondo en onboarding

--- src/views/onboarding/Welcome.vue
+++ src/views/onboarding/Welcome.vue
@@ -36,7 +36,7 @@ export default {
   bottom: 0;
   width: 100%;
   height: 100%;
-  background-color: var(--color-neutral-lightest);
+  background-color: var(--color-brand-lightest);
   z-index: 100000;
 
   .container {
