1461281d0475ba43019bc40a036d2f71c51bd1bd
Basilio Cáceres <basilio@bloom-ux.com>
2020-07-14 10:22:19 -0400

    nuevas vistas, page.scss

---
+++ src/views/NewAidIntro2.vue
@@ -0,0 +1,31 @@
+<!-- eslint-disable max-len -->
+<template>
+  <div class="page">
+    <div class="container">
+      <page-image alt="Persona activando el audio en la aplicación" class="onboarding__welcome-image"></page-image>
+      <h2 class="page__title">
+        Título
+        <text-to-speech :text-audio="'Audio'" />
+      </h2>
+      <p>Contenido</p>
+      <footer class="page__footer">
+        <OnboardingNext :linkTo="'/path/'" />
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import OnboardingNext from '@/components/OnboardingNext.vue';
+import TextToSpeech from '@/components/TextToSpeech.vue';
+import PageImage from '../../public/img/illustrations/onboarding-activar-audio.svg?inline';
+
+export default {
+  name: 'Welcome',
+  components: {
+    OnboardingNext,
+    TextToSpeech,
+    PageImage,
+  },
+};
+</script>
