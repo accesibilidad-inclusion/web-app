a14bf50d2f0ed1204b12a80ff8652e92f3b38373
Diego Arenas <jacodiego@hotmail.com>
2020-09-28 12:27:02 -0500

    test service worker

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = 'pictos-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/offline.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(FILES_TO_CACHE);
+      }),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keyList => Promise.all(keyList.map((key) => {
+      if (key !== CACHE_NAME) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        return caches.delete(key);
+      }
+      return null;
+    }))),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode !== 'navigate') {
+    return;
+  }
+  event.respondWith(
+    fetch(evt.request)
+      .catch(() => caches.open(CACHE_NAME)
+        .then(cache => cache.match('offline.html'))),
+  );
+});
