54cfb8b298a1a5e03134bad54571c1e0a9fd0687
Cindy Sanhueza <cindy.sanhueza2011@gmail.com>
2020-09-30 17:14:53 -0300

    Sin resultados de búsqueda

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -16,6 +16,14 @@
       </template>
       <template v-else>
         <template v-if="!tasks.length">
+          <div class="search-no-results">
+            <div class="search-no-results__content">
+              <icon-no-results class="search-no-results__icon" />
+              <h2 class="search-no-results__title">Búsqueda sin resultados</h2>
+              <p class="search-no-results__description">Prueba buscando con <strong>otras palabras</strong> o usando las <strong>categorías</strong> disponibles</p>
+            </div>
+            <router-link to="/home" class="btn btn--primary btn--large btn--block" tag="button">Volver a Inicio</router-link>
+          </div>
         </template>
         <template v-else>
           <task-block v-for="task in tasks"
@@ -31,6 +39,7 @@
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue';
 import TextToSpeech from '@/components/TextToSpeech.vue';
 import TaskBlock from '@/components/TaskBlock.vue';
+import IconNoResults from '../../public/img/app-icons/no-results.svg?inline';
 
 export default {
   name: 'searchResults',
@@ -38,6 +47,7 @@ export default {
     ClipLoader,
     TextToSpeech,
     TaskBlock,
+    IconNoResults,
   },
   data() {
     return {
@@ -62,6 +72,9 @@ export default {
 
 <style lang="scss">
   @import '@/assets/scss/rfs.scss';
+  .search-results {
+    position: relative;
+  }
   .search-results__description {
     margin-bottom: var(--spacer);
     padding: var(--spacer-sm) var(--spacer) var(--spacer);
@@ -101,6 +114,57 @@ export default {
     @media screen and ( min-width: 1280px ) {
       padding-left: var(--spacer-xl);
       padding-right: var(--spacer-xl);
+    }
+  }
+  /*sin resultados*/
+  .search-no-results {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    bottom: var(--spacer);
+    width: 100%;
+    padding: 0 var(--spacer);
+    left: 0;
+    top: 100px;
+    @media screen and ( min-width: 640px ) {
+      max-width: 560px;
+      left: auto;
+      right: auto;
+      padding: 0;
+      top: 130px;
+    }
+    @media screen and ( min-width: 1280px ) {
+      max-width: 630px;
+      top: 140px;
+    }
+  }
+  .search-no-results__content {
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
+  .search-no-results__icon {
+    width: var(--spacer-lg);
+    height: var(--spacer-lg);
+    margin: var(--spacer) auto;
+    display: block;
+  }
+  .search-no-results__title {
+    @include rfs($font-size-18);
+    color: var(--color-brand);
+    margin-bottom: var(--spacer-sm);
+    text-transform: uppercase;
+    font-weight: 800;
+    text-align: center;
+  }
+  .search-no-results__description {
+    @include rfs($font-size-14);
+    text-align: center;
+    max-width: 240px;
+    @media screen and ( min-width: 640px ) {
+      max-width: 350px;
     }
   }
 </style>
