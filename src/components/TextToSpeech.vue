7ade6e1bec5d274678b1e6821f9454a3db0a051c
Diego Arenas <jacodiego@hotmail.com>
2023-04-10 12:50:34 -0600

    update speech

--- src/components/TextToSpeech.vue
+++ src/components/TextToSpeech.vue
@@ -1,7 +1,8 @@
 <template>
-  <button class="tts" @click.prevent="speak" type="button">
+  <button class="tts" @click.stop="speak" type="button">
     <span class="sr-only">Leer texto</span>
-    <icon-audio />
+    <icon-audio v-if="!playing" />
+    <icon-stop class="stop-img" v-if="playing" />
   </button>
 </template>
 
@@ -10,35 +11,50 @@ import {
   Component, Prop, Vue, Ref,
 } from 'vue-property-decorator';
 import IconAudio from '../../public/img/app-icons/audio.svg?inline';
+import IconStop from '../../public/img/app-icons/stop-circle.svg?inline';
 
 export default {
   props: ['textAudio'],
   components: {
     IconAudio,
+    IconStop,
   },
   mounted() {
     this.voiceList = this.synth.getVoices();
     this.synth.onvoiceschanged = () => {
       this.voiceList = this.synth.getVoices();
     };
+
+    this.utterance.lang = 'es-MX';
+    this.utterance.pitch = 1;
+    this.utterance.rate = 1;
+    this.utterance.localService = true;
+    this.utterance.voice = this.voiceList.find(voice => voice.lang === 'es-MX' || voice.lang === 'spa-MEX');
+
+    this.utterance.onstart = (event) => {
+      this.playing = true;
+    };
+    this.utterance.onend = (event) => {
+      this.playing = false;
+    };
   },
   data() {
     return {
+      playing: false,
       synth: window.speechSynthesis,
       voiceList: [],
-      greetingSpeech: new window.SpeechSynthesisUtterance(),
+      utterance: new SpeechSynthesisUtterance(),
     };
   },
   methods: {
     speak() {
-      this.greetingSpeech.text = this.textAudio;
-      this.greetingSpeech.lang = 'es-MX';
-      this.greetingSpeech.pitch = 1;
-      this.greetingSpeech.rate = 1;
-      this.greetingSpeech.localService = true;
-      this.greetingSpeech.voice = this.voiceList.find(voice => voice.lang === 'es-MX' || voice.lang === 'spa-MEX');
-      this.synth.cancel();
-      this.synth.speak(this.greetingSpeech);
+      if (this.playing) {
+        this.playing = false;
+        this.synth.cancel();
+      } else {
+        this.utterance.text = this.textAudio;
+        this.synth.speak(this.utterance);
+      }
     },
   },
 };
@@ -59,7 +75,26 @@ export default {
   @include rfs(1rem, width);
   @include rfs(1rem, height);
 }
+
+.tts ::v-deep .stop-img {
+  animation: pulse 1s linear infinite;
+}
 .tts ::v-deep path {
   fill: var(--color-brand);
+}
+
+@keyframes pulse {
+  0% {
+      -webkit-transform: scale(1.2);
+      transform: scale(1.2);
+  }
+  50% {
+    -webkit-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+  100% {
+      -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
 }
 </style>
