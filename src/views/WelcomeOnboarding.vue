9cb679f5f33c16ab4c7408a3d78df2b37492020d
Diego Arenas <jacodiego@hotmail.com>
2023-10-18 14:09:31 -0600

    cargar imagenes de onboarding sin async

--- src/views/WelcomeOnboarding.vue
+++ src/views/WelcomeOnboarding.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import OnboardingNav from '@/components/OnboardingNav.vue'
+import OnboardingImage1 from '@/assets/img/illustrations/activar-tutoriales 1.svg?component'
+import OnboardingImage2 from '@/assets/img/illustrations/activar-tutoriales 2.svg?component'
+import OnboardingImage3 from '@/assets/img/illustrations/activar-tutoriales 3.svg?component'
+import OnboardingImage4 from '@/assets/img/illustrations/activar-tutoriales 4.svg?component'
 
 import { useAppNavStore } from '@/stores/app-nav.js'
 import type { Onboarding } from '@/types/onboarding'
-import { defineAsyncComponent } from 'vue'
 
 const appNav = useAppNavStore()
 
@@ -11,30 +14,22 @@ const sequence: Array<Onboarding> = [
   {
     title: 'Activa el audio',
     body: 'Si tienes dificultad o no quieres leer, no te preocupes. Puedes <b>activar la opción de sonido, pulsando el ícono de altavoz</b>.',
-    image: defineAsyncComponent(
-      () => import('@/assets/img/illustrations/activar-tutoriales 1.svg?component')
-    )
+    image: OnboardingImage1
   },
   {
     title: '¿Para qué sirve PICTOS?',
     body: 'Pictos te ayudará a realizar tareas en distintos servicios, mostrándote una lista de todas las cosas que puedes hacer en cada lugar.',
-    image: defineAsyncComponent(
-      () => import('@/assets/img/illustrations/activar-tutoriales 2.svg?component')
-    )
+    image: OnboardingImage2
   },
   {
     title: '¿Cómo funciona?',
     body: 'Pictos te explica paso a paso qué puedes hacer en cada lugar, a través de pictogramas.',
-    image: defineAsyncComponent(
-      () => import('@/assets/img/illustrations/activar-tutoriales 3.svg?component')
-    )
+    image: OnboardingImage3
   },
   {
     title: '¡Tú nos puedes ayudar!',
     body: 'Queremos entregar a la comunidad las herramientas para apoyarnos entre todos.',
-    image: defineAsyncComponent(
-      () => import('@/assets/img/illustrations/activar-tutoriales 4.svg?component')
-    )
+    image: OnboardingImage4
   }
 ]
 </script>
