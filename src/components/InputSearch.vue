e9d93aa20ffa12178300d45fe4985c7bba67de3f
Cindy Sanhueza <cindy@bloom-ux.com>
2024-01-09 18:33:07 -0300

    Icono en input buscar y ajuste en custom control

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -15,7 +15,7 @@ defineEmits(['search', 'update:modelValue'])
 </script>
 
 <template>
-  <div class="input-search__group">
+  <div class="input-search__group custom-control">
     <input
       id="s"
       :value="modelValue"
@@ -23,51 +23,39 @@ defineEmits(['search', 'update:modelValue'])
       class="input-search__input"
       type="text"
       placeholder="Ejemplo: Viajar en metro" />
-    <icon-search class="input-search__icon" @click="$emit('search')" />
+      <span class="input-search__icon">
+        <icon-search @click="$emit('search')" />
+      </span>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/scss/rfs.scss';
 .input-search__group {
-  position: relative;
-  margin: var(--spacer--500) 0;
+  margin: var(--spacer--400) 0;
 }
 .input-search__input {
-  @include rfs($font-size-16);
-  width: 100%;
-  padding: var(--spacer--500);
-  border: 1px solid var(--color--blue-dark);
-  border-radius: var(--spacer--500);
-  &::placeholder {
-    color: #848484;
-    opacity: 1;
-    font-style: italic;
-    font-family: inherit;
-  }
+  position: relative;
 }
 .input-search__icon {
   cursor: pointer;
   position: absolute;
-  top: 50%;
-  right: var(--spacer--500);
   background: none;
   border: none;
-  z-index: 9;
-  width: var(--font-size--500);
-  height: var(--font-size--500);
-  transform: translateY(-50%);
-  path {
-    fill: var(--color--blue);
-  }
-}
-@media screen and (max-width: 640px) {
-  input[type='email'],
-  input[type='search'],
-  input[type='text'],
-  select:focus,
-  textarea {
-    font-size: 16px;
+  top: 0;
+  right: 0;
+  width: 64px;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: var(--spacer--500);
+  svg {
+    width: 16px;
+    height: 16px;
+    &:deep(path) {
+      fill: var(--color--blue);
+    }
   }
 }
 </style>
